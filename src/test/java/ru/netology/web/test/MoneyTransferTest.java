e8d3ab5f7f5a712ed4bb4a59a2efe189c35940ce
Anastasia <n41111215@mail.ru>
2021-03-03 14:27:45 +0300

    add positive test

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -3,16 +3,28 @@ package ru.netology.web.test;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
 import ru.netology.web.page.LoginPage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MoneyTransferTest {
-    @Test
-    void shouldTransferMoneyBetweenOwnCardsV1() {
+    private int amountValid = 500;
+
+    private DashboardPage shouldOpenDashboardPage() {
         open("http://localhost:9999");
         val loginPage = new LoginPage();
-//    val loginPage = open("http://localhost:9999", LoginPageV1.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        return verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldOpenValidLogin() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPage();
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
         val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
@@ -20,22 +32,20 @@ public class MoneyTransferTest {
     }
 
     @Test
-    void shouldTransferMoneyBetweenOwnCardsV2() {
-        open("http://localhost:9999");
-        val loginPage = new LoginPage();
-//    val loginPage = open("http://localhost:9999", LoginPageV2.class);
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
+    void shouldTransferMoneyFromCard2toCard1() {
+        DashboardPage dashboardPage = shouldOpenDashboardPage();
+        dashboardPage.dashboardPageVisible();
+        int expected1 = dashboardPage.getBalanceCard1() + amountValid;
+        int expected2 = dashboardPage.getBalanceCard2() - amountValid;
+        val moneyTransfer = dashboardPage.card1();
+        moneyTransfer.moneyTransferVisible();
+        moneyTransfer.setTransferAmount(amountValid);
+        moneyTransfer.setFrom(DataHelper.getCardNumber2());
+        moneyTransfer.doTransfer();
+        assertEquals(expected1, dashboardPage.getBalanceCard1());
+        assertEquals(expected2, dashboardPage.getBalanceCard2());
     }
 
-    @Test
-    void shouldTransferMoneyBetweenOwnCardsV3() {
-        val loginPage = open("http://localhost:9999", LoginPage.class);
-        val authInfo = DataHelper.getAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-        verificationPage.validVerify(verificationCode);
-    }
 }
+
+
