b2bc8a5140d9d9785da9014c8877042044634310
Anastasia <n41111215@mail.ru>
2021-03-03 15:25:24 +0300

    edit css selector button

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -2,14 +2,16 @@ package ru.netology.web.page;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
     private SelenideElement heading = $("[data-test-id=dashboard]");
 
-    private SelenideElement card1Button = $("[data-test-id=action-deposit]");
-    private SelenideElement card2Button = $("[data-test-id=action-deposit]");
+    private SelenideElement card1Button = $$(By.className("button_view_extra")).first();
+    private SelenideElement card2Button = $$(By.className("button_view_extra")).last();
     private SelenideElement balanceCard1 = $("[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']");
     private SelenideElement balanceCard2 = $("[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
 
